559a846ca6cf688c713db319756d5a59a7062a41
mandy <mandy@DESKTOP-LPQPPK8>
2020-06-13 16:58:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Sumador
+{
+    static class Program
+    {
+        
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            int a = 0, b = 0; int sumaab = 0,x=0;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(5,9,sumaab));
+            x = sumaab;
+            
+        }
+    }
+}
