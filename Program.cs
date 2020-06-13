b88b129e869c20381d7003a7fb2260c630d892df
Delta-dev-99 <delta.deltadev.127@gmail.com>
2020-06-13 17:55:53 -0400

    Set new main as default

--- Program.cs
+++ Program.cs
@@ -9,29 +9,11 @@ namespace Sumador
 {
 	static class Program
 	{
-
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-			int a;
-			int b;
-			int sumaab;
-
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1(5, 9, &sumaab));
-		}
-
 		/* 
             Un segundo Main para que pruebes con el form que hice.
-            Para que inicie por aca le puedes intercambiar el nombre con el otro main
-            o llamar a este directo si estas llamando desde C++
         */
 		[STAThread]
-		static void _Main()
+		static void Main()
 		{
 			int a, b;
 			int suma;
@@ -41,6 +23,23 @@ namespace Sumador
 
 			ResultForm formulario = new ResultForm(a, b);
 			suma = formulario.show_get_result();    //Esta funcion muestra el form y espera a que se cierre y devuelve la suma
+		}
+
+
+
+        /// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main_disabled() //Le cambie el nombre para que el otro sea llamado por defecto
+		{
+			int a;
+			int b;
+			int sumaab;
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1(5, 9, &sumaab));
 		}
 	}
 }
